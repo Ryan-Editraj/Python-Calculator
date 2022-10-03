4ebd25c2f40e8743b3b45467200a78e018764e86
Ryan <103019033+Ryan-techie@users.noreply.github.com>
2022-10-03 06:49:21 +0530

    Update PythonCalculator.py

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -1,4 +1,19 @@
 print('Helloworld')
 print('Python calculator')
 
+num1 = int(input('>'))
+operator = input('>')
+num2 = int(input('>'))
 
+
+if operator == '+':
+  print(num1 + num2)
+  
+if operator == '-':
+  print(num1 - num2)
+  
+if operator == '/':
+  print(num1 / num2)
+  
+if operator == '*':
+  print(num1 * num2)

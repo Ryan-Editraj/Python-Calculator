9e3f0fa527962949a012abbe85242f1afdafa806
Ryan <103019033+ryaneditraj@users.noreply.github.com>
2023-10-08 13:10:51 +0530

    Update PythonCalculator.py

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -16,3 +16,9 @@ def divide(num1, num2):
 # Define additional mathematical functions
 def sin(x):
     return math.sin(x)
+
+def cos(x):
+    return math.cos(x)
+
+def tan(x):
+    return math.tan(x)

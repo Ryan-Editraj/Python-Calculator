785632c62ce0f1dbaeb480bd2aa2e736706c2af6
Ryan Editraj <114290745+Ryan-Editraj@users.noreply.github.com>
2023-10-08 13:09:27 +0530

    Update PythonCalculator.py

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -1,18 +1,1 @@
-import math
-
-# Define the four basic arithmetic functions
-def add(num1, num2):
-    return num1 + num2
-
-def subtract(num1, num2):
-    return num1 - num2
-
-def multiply(num1, num2):
-    return num1 * num2
-
-def divide(num1, num2):
-    return num1 / num2
-
-# Define additional mathematical functions
-def sin(x):
-    return math.sin(x)
+print("THIS IS CALCULATOR")
